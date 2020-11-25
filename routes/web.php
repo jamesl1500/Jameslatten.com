380427fe1239030e179ef9208102f11d86980c14
James Latten <billing@sitelyftstudios.com>
2020-11-25 17:17:10 -0500

    Blairritchey category test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,50 @@
 Route::get('/', function () {
     return view('index');
 })->name('index.index');
+
+Route::get('/categories/{id}', function($id)
+{
+    if($id != "")
+    {
+        // Connect
+        $url = "https://api.bigcommerce.com/stores/j2fozjob/v3/";
+
+        $client_id = "7inkiy19zw2afn0fsglaq0ew6r073w6";
+        $client_secret = "1c7e604c8f7494457b450e42e79c67b1c4c8030e5f35f7e5fe80b9e0bcbbd9a6";
+        $access_token = "29rrdtxf3z0swjd7aa9sq1krkgfx0fd";
+
+        // Curl
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.bigcommerce.com/stores/j2fozjob/v3/catalog/products",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+              "x-auth-token: " . $access_token
+            ),
+        ));
+
+        // Test
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+    }
+
+});
+
+Route::get('/categories/{id}/image', function(){
+
+});
+
